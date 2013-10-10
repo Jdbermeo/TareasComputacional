2d03e0ae767e0516fa87fef768d2f1e426f13d6a
Juan Diego Bermeo <jd.bermeo251@uniandes.edu.co>
2013-10-09 19:13:22 -0500

    cd

---
+++ tarea_5/plot_senal.py
@@ -0,0 +1,16 @@
+def plot_señal(señal_original,señal_reconstruida):
+    
+    """
+    
+    Esta función plotea los puntos que constituyen las señales originales y la reconstruida a través de la transformada de Fourier.
+
+    Input:
+    - señal_original: arreglo numpy con los datos de cada señal original, de tamaño [400,24].
+    - señal_reconstruida: arreglo numpy con los datos de cada señal reconstruida, de tamaño [400,24].
+
+    Output:
+    gráfica donde se comparan la primera señal original con la primera señal reconstruida.
+
+    """
+
+    return
