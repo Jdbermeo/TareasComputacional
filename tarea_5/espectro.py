5ae154528fe32b0a6eefc27353561df09ed53994
Juan Diego Bermeo <jd.bermeo251@uniandes.edu.co>
2013-10-10 09:23:30 -0500

    tarea 5

--- tarea_5/espectro.py
+++ tarea_5/espectro.py
@@ -2,17 +2,17 @@ def espectro_potencias(fft_x,fft_freq):
 
      """
      
-     Esta función calcula el espectro de potencias de cada señal y devuelve los 10 valores màs grandes de amplitud con su respectiva frecuencia.
+     Esta funcion calcula el espectro de potencias de cada senal y devuelve los 10 valores mas grandes de amplitud con su respectiva frecuencia.
 
      Input:
-     - fft_freq: arreglo numpy de tamaño [400,24]. Frecuencias de cada señal.
-     - fft_x: arreglo numpy de números complejos de tamaño [400,24]. Amplitudes de cada señal.
+     - fft_freq: arreglo numpy de tamano [400,24]. Frecuencias de cada senal.
+     - fft_x: arreglo numpy de numeros complejos de tamano [400,24]. Amplitudes de cada senal.
 
      Output:
-     - max_espectro_x: arreglo numpy con los 10 valores de fft_x normalizados más grandes de tamaño [400,24].
-     - max_espectro_freq: arreglo numpy con los 10 valores correspondientes a valor de max_espectro_x de tamaño[400,24].
+     - max_espectro_x: arreglo numpy con los 10 valores de fft_x normalizados mas grandes de tamano [400,24].
+     - max_espectro_freq: arreglo numpy con los 10 valores correspondientes a valor de max_espectro_x de tamano[400,24].
 
-     24 corresponde a las señales.
+     24 corresponde a las senales.
      400 corresponde a las frecuencias.
 
      """
